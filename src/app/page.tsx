9ca30a1b37ae5d1bba81ca31fb9b2476e538d172
Sunil H N <sunilh.n27@gmail.com>
2024-05-03 00:17:33 +0530

    Changes in Page class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+import FeaturesFour from "@/components/FeaturesFour";
+import FeaturesOne from "@/components/FeaturesOne";
+import FeaturesThree from "@/components/FeaturesThree";
+import FeaturesTwo from "@/components/FeaturesTwo";
+import Hero from "@/components/Hero";
+import NavBar from "@/components/NavBar";
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <NavBar />
+      <Hero />
+      <FeaturesOne />
+      <FeaturesTwo />
+      <FeaturesThree />
+      <FeaturesFour />
+    </main>
+  );
 }
