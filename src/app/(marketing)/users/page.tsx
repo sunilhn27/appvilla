310135b37ff68ac78c31ee992602e5a5085b4c11
Sunil H N <sunilh.n27@gmail.com>
2024-05-14 00:10:21 +0530

    added users with interface

--- src/app/(marketing)/users/page.tsx
+++ src/app/(marketing)/users/page.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
-function UserPage() {
-  return <div>UserPage</div>;
+interface Users {
+  name: string;
+  id: number;
+  email: string;
+}
+
+async function UserPage() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: Users[] = await data.json();
+
+  return (
+    <>
+      <div>
+        <h1 className="text-[2rem]">UserPage</h1>
+        <ol>
+          {users.map((user) => (
+            <>
+              <li>
+                {user.id}
+                {user.name} {user.email}
+              </li>
+            </>
+          ))}
+        </ol>
+      </div>
+    </>
+  );
 }
 
 export default UserPage;
