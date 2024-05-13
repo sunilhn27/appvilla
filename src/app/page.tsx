b9ddab0bcfd33ede69309bad414feb90b91f20ce
Sunil H N <sunilh.n27@gmail.com>
2024-05-13 23:19:04 +0530

    Changes in Page Class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Accordion from "@/components/Accordion";
+import AyroUI from "@/components/AyroUI";
 import FeaturesFour from "@/components/FeaturesFour";
 import FeaturesOne from "@/components/FeaturesOne";
 import FeaturesThree from "@/components/FeaturesThree";
 import FeaturesTwo from "@/components/FeaturesTwo";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import NavBar from "@/components/NavBar";
+import OurTeam from "@/components/OurTeam";
+import Pricing from "@/components/Pricing";
+import UseApp from "@/components/UseApp";
 import Image from "next/image";
 
 export default function Home() {
@@ -15,6 +21,12 @@ export default function Home() {
       <FeaturesTwo />
       <FeaturesThree />
       <FeaturesFour />
+      <Pricing />
+      <OurTeam />
+      <AyroUI />
+      <Accordion />
+      <UseApp />
+      <Footer />
     </main>
   );
 }
