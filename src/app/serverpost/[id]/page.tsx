ba08906f07b544a4c786956dab13fc231021c08f
Sunil H N <sunilh.n27@gmail.com>
2024-05-14 00:11:29 +0530

    Changes in server post

--- src/app/serverpost/[id]/page.tsx
+++ src/app/serverpost/[id]/page.tsx
@@ -1,6 +1,8 @@
 //ServerClient
-export default function postID({ params }: any) {
+export default function postID({ params ,searchParams}: any) {
   console.log(params.id);
+
+  console.log(searchParams.name)
   return (
     <>
       <h1>Post iD {params.id}</h1>
